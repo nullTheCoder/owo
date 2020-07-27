a204b55ecdb3803e8f4feeac309e45663e5f3a09
KonstantinTolstoy <kanstancin.talstoj@gmail.com>
2020-07-27 17:28:27 +0300

    minor fixes

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-#    OWO is a minecraft trash resourcepack converter generator
-#    Copyright (C) 2020 Konstantin Tolstoy & Stepan Gaidukevich
+#  OWO is a minecraft trash resourcepack converter generator
+#  Copyright (C) 2020 Konstantin Tolstoy & Stepan Gaidukevich
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
 import random
@@ -48,7 +48,14 @@ def handle_localization():
     i = inp + "lang/"
     o = outp + "lang/"
 
-    f = open(i + "en_US.lang", "r")
+    if os.path.isfile(i + "en_US.lang"):
+        f = open(i + "en_US.lang", "r")
+    elif os.path.isfile("./en_US.lang"):
+        print("United States English localization not found in input resourcepack. Trying to use built-in localization (1.8.9)")
+        f = open("./en_US.lang")
+    else:
+        print("No localizations found")
+        return
 
     endLines = []
 
@@ -65,7 +72,7 @@ def handle_localization():
             
         if "=" in line:
             x = line.split("=")
-            y = x[1].replace("r", "w").replace("t", "h").replace("sh", "h").replace("diamond", "owo cryftal")      
+            y = x[1].replace("r", "w").replace("t", "h")
             res = x[0] + "=" + y.rstrip("\n") + " " + a
             
         else:
@@ -86,6 +93,10 @@ def handle_localization():
 def handle_blocks():
     i = inp + "textures/blocks/"
     o = outp + "textures/blocks/"
+
+    if not os.path.isdir(i):
+        print("Block textures not found in input texturepack")
+        return
 
     a = os.listdir(i)
     a2 = os.listdir("./uwus/")
@@ -109,8 +120,3 @@ def handle_blocks():
 
 
 main()
-
-
-
-
-
